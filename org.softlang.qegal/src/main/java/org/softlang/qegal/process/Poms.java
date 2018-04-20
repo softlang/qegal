dddcaa7b4c8882e1d9da1556a99f1c59b5368946
johanneshaertel <johanneshaertel@uni-koblenz.de>
2018-04-20 12:43:41 +0200

    things

---
+++ org.softlang.qegal/src/main/java/org/softlang/qegal/process/Poms.java
@@ -0,0 +1,116 @@
+package org.softlang.qegal.process;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.io.IOUtils;
+import org.softlang.qegal.jutils.JUtils;
+
+import com.google.common.base.Charsets;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+
+public class Poms {
+
+	public static final int GIT_INDEXED_MAX_SIZE = 384000;
+
+	public static void main(String[] args) throws Exception {
+		queryPoms();
+	}
+
+	public static void queryPoms() throws Exception {
+		String query = "https://api.github.com/search/code?q=filename:built.sbt+extension:sbt";
+
+		int totalCount = collect(query).getAsJsonObject().get("total_count").getAsInt();
+
+		File file = new File("data/repositories123.csv");
+
+		CSVPrinter printer = CSVFormat.DEFAULT.print(file, Charsets.UTF_8);
+		printer.printRecord("total", "low", "high", "page", "repository", "url");
+		collectAll(printer, query, 0, GIT_INDEXED_MAX_SIZE);
+		printer.close();
+	}
+
+	public static void collectAll(CSVPrinter printer, String service, int low, int high) throws Exception {
+		for (int page = 1; page <= 10; page++) {
+			String query = service + "+size:" + String.valueOf(low) + ".." + String.valueOf(high) + "&page="
+					+ String.valueOf(page) + "&per_page=100";
+			JsonObject json = collect(query).getAsJsonObject();
+			int totalCount = json.get("total_count").getAsInt();
+
+			if (totalCount > 1000 && low != high) {
+				System.out.println("Spliting " + totalCount + " @ " + query);
+				int middle = low + (int) Math.floor(((double) (high - low)) / 2.0);
+				collectAll(printer, service, low, middle);
+				collectAll(printer, service, middle + 1, high);
+				return;
+			}
+
+			System.out.println("Consuming " + totalCount + " @ " + query);
+
+			for (JsonElement item : json.get("items").getAsJsonArray()) {
+				String fullName = item.getAsJsonObject().get("repository").getAsJsonObject().get("full_name")
+						.getAsString();
+				String url = item.getAsJsonObject().get("html_url").getAsString();
+				printer.printRecord(totalCount, low, high, page, fullName, url);
+			}
+			printer.flush();
+
+			// No results left here.
+			if ((page) * 100 > totalCount)
+				return;
+		}
+	}
+
+	public static JsonPrimitive rateLimit() {
+		return collect("https://api.github.com/rate_limit").getAsJsonPrimitive();
+	}
+
+	public static JsonElement collect(String service) {
+		try {
+			URL myURL = new URL(service);
+			HttpURLConnection myURLConnection = (HttpURLConnection) myURL.openConnection();
+
+			String userCredentials = JUtils.configuration("login_git") + ":" + JUtils.configuration("password_git");
+			String encodedAuthorization = "Basic " + new String(org.apache.commons.codec.binary.Base64
+					.encodeBase64(userCredentials.getBytes(Charsets.UTF_8.name())), Charsets.UTF_8.name());
+
+			myURLConnection.setRequestProperty("Authorization", encodedAuthorization);
+			myURLConnection.connect();
+
+			int response = myURLConnection.getResponseCode();
+
+			if (200 == response) {
+				JsonElement jelement = new JsonParser().parse(new InputStreamReader(myURLConnection.getInputStream()));
+
+				// Process this.
+				myURLConnection.disconnect();
+				return jelement;
+			} else if (403 == response) {
+				System.out.println("403 GiThUb");
+				Thread.sleep(30000l);
+				return collect(service);
+			} else {
+				throw new RuntimeException(
+						"GiThUb: " + IOUtils.toString(myURLConnection.getErrorStream(), Charsets.UTF_8));
+			}
+		} catch (Exception e) {
+			System.out.println("exception and retrying" + e);
+			try {
+				Thread.sleep(30000l);
+			} catch (InterruptedException e1) {
+				throw new RuntimeException(e1);
+			}
+			return collect(service);
+		}
+
+	}
+
+}
