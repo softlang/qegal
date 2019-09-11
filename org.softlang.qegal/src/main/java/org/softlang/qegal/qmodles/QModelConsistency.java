198e063ce93760cd0c46f69334ea03101151c698
Marcel <heinz@uni-koblenz.de>
2019-09-11 12:55:40 +0200

    consistency

--- org.softlang.qegal/src/main/java/org/softlang/qegal/qmodles/QModelConsistency.java
+++ org.softlang.qegal/src/main/java/org/softlang/qegal/qmodles/QModelConsistency.java
@@ -1,17 +1,91 @@
 package org.softlang.qegal.qmodles;
 
-public class QModelConsistency {
+import java.io.File;
+import java.util.Set;
+import java.util.function.Consumer;
 
-	public static void main(String[] args) {
-		// load .ttl
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
+import static org.softlang.qegal.utils.QegalUtils.query;
+
+public class QModelConsistency {
+	
+	public static void main(String stagepath) {
+		String[] existence = {ecoreFiles(),generatorFiles(),eClassInterfaces(),eClassImpls()};
+		String[] negative = {};
+		String[][] countequal = {{ecoreFiles(),generatorFiles()},{eClassInterfaces(),eClassImpls()}};
 		
-		//rule1: there exists at least 1 Ecore file
-		//rule2: there exists at least 1 Genmodel
-		//rule3: for every Ecore file, there exists a Genmodel
-		//rule4: for every genmodel, there exists generated code
-		//rule5: for every interface, there exists an implementation
-		//rule6: for every genclass, there exists an interface
-		//rule7: for every structural feature, there exists a variable, getter and setter
+		// load .ttl
+		File ttls = new File(stagepath+"/ttls/");
+		for(File f:ttls.listFiles(f -> f.getName().endsWith(".ttl"))) {
+			Model model = RDFDataMgr.loadModel(f.getAbsolutePath());
+			
+			for(String querytext : existence) {
+				Set<String> result = query(model, querytext);
+				if(result.isEmpty()) {
+					System.err.println("Existence Error");
+					System.err.println(querytext);
+				}
+			}
+			for(String[] querytext : countequal) {				
+				String query1 = querytext[0];
+				String query2 = querytext[1];
+				if(query(model, query1).size()!=query(model,query2).size()) {
+					System.err.println("Count error");
+					System.err.println(query1);
+					System.err.println(query2);
+				};
+			}
+		}
+	}
+	
+	/**
+	 * get Ecore files
+	 * @return SPARQL
+	 */
+	private static String ecoreFiles() {
+		String query = "\"PREFIX sl: <http://org.softlang.com/> \n"
+				+ "SELECT DISTINCT ?x WHERE {"
+				+ "	?x sl:elementOf sl:EcoreXMI . "
+				+ "}";
+		return query;
+	}
+	
+	/**
+	 * get Generator model
+	 * @return SPARQL
+	 */
+	private static String generatorFiles() {
+		String query = "\"PREFIX sl: <http://org.softlang.com/> \n"
+				+ "SELECT DISTINCT ?x WHERE {"
+				+ "	?x sl:elementOf sl:GeneratorXMI . "
+				+ "}";
+		return query;
+	}
+	
+	/**
+	 * get EClass Interface
+	 * realized as: there 
+	 * @return
+	 */
+	private static String eClassInterfaces() {
+		String query = "\"PREFIX sl: <http://org.softlang.com/> \n"
+				+ "SELECT DISTINCT ?x WHERE {"
+				+ "	?x sl:elementOf sl:EClassInterfaceJava . "
+				+ "}";
+		return query;
 	}
 
+	/**
+	 * get EClass Impl
+	 * realized as: there 
+	 * @return
+	 */
+	private static String eClassImpls() {
+		String query = "\"PREFIX sl: <http://org.softlang.com/> \n"
+				+ "SELECT DISTINCT ?x WHERE {"
+				+ "	?x sl:elementOf sl:EClassImplJava . "
+				+ "}";
+		return query;
+	}
 }
